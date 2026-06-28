21db7f5a425ab9a1ef822491bba997ef20e15f2e
Zafkii <tapiadavid104@gmail.com>
2026-06-28 17:29:13 -0400

    ft1

--- animprite/internal/editor/import.go
+++ animprite/internal/editor/import.go
@@ -5,7 +5,9 @@ func (a *EditorApp) openMovementFile(path string) error {
 	if err != nil {
 		return err
 	}
+	savedSprites := a.proj.Sprites
 	a.proj = *proj
+	a.proj.Sprites = savedSprites
 	a.rebuildFromProj()
 	return nil
 }
